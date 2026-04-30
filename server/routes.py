050956d9de1f488c48342c1d7c819acaa8cd78db
Kush Patel <kushpatel.personal@gmail.com>
2026-04-30 21:20:39 +0800

    updated routes for login and register of user

--- server/routes.py
+++ server/routes.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, flash
+from server.forms import LoginForm, RegistrationForm
+from server.models import User
+from server import db
+
 
 main = Blueprint('main', __name__)
 
@@ -18,14 +22,31 @@ def scoreboard():
     return render_template('scoreboard.html')
 
 
-@main.route('/login')
+@main.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('main.login'))
+        flash(f'Successfully logged in as {form.username.data}')
+        return redirect(url_for('main.index'))
+    return render_template('login.html', form=form)
 
 
-@main.route('/register')
+@main.route('/register', methods=['GET', 'POST']) 
 def register():
-    return render_template('register.html')
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user=User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        
+        flash(f'Registration requested for user {form.username.data}')
+        return redirect(url_for('main.index'))
+    return render_template('register.html', form=form)
 
 
 @main.route('/logout')
