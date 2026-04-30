3871a0c51bff4d54f0fda44dd2679adc4506bad7
Kush Patel <kushpatel.personal@gmail.com>
2026-04-30 20:21:26 +0800

    implemented flask-wtf login and registration form

---
+++ server/forms.py
@@ -0,0 +1,15 @@
+# server/forms.py
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Create Account')
