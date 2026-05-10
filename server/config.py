9ce5b1afbde8ea6bad9e05f7730ef49d25b3686b
Kush Patel <kushpatel.personal@gmail.com>
2026-05-10 18:52:06 +0800

    Set up TMDB api keys and paths

--- server/config.py
+++ server/config.py
@@ -6,3 +6,7 @@ default_database_location = 'sqlite:///' + os.path.join(basedir, 'app.db')
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or default_database_location
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-later'
+
+    TMDB_API_KEY = os.environ.get('TMDB_API_KEY')
+    TMDB_BASE = 'https://api.themoviedb.org/3'
+    POSTER_BASE = 'https://image.tmdb.org/t/p/w500'
