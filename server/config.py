17655f7fff802e038faefece66450b6857ad2b3b
flamedphoenix <deankalweit@gmail.com>
2026-05-10 12:54:55 +0800

    security: remove hardcoded fallback SECRET_KEY (CWE-798)

--- server/config.py
+++ server/config.py
@@ -5,4 +5,6 @@ default_database_location = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or default_database_location
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-later'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    if not SECRET_KEY:
+    	raise RuntimeError('SECRET_KEY environment variable is not set')
